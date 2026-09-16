d98cbc7940bf60308a4ad81ff64dec070f394473
Ddm1234 <dharamm663@gmail.com>
2026-09-16 05:56:48 +0530

    Inspect OpenSea eligibility stages

--- opensea-tracker.js
+++ opensea-tracker.js
@@ -352,6 +352,7 @@ async function checkWallet(wallet, accessToken, drops) {
       );
 
       console.log(`[${wallet.name}] ${slug} eligibility keys:`, Object.keys(eligibility).join(", "));
+    console.log(`[${wallet.name}] ${slug} stages:`, JSON.stringify(eligibility.stages));
       const eligibleStages =
         eligibility.stages ||
         eligibility.eligibleStages ||
